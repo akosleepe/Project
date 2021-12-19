9910f611ef78e0ff49a579bfe9ca81f65f8a8bea
akosleepe <leolastleo@gmail.com>
2021-12-19 20:05:54 +0700

    users_table v 0.1

--- lib/users_table.cpp
+++ lib/users_table.cpp
@@ -1,14 +1,81 @@
 #include "users_table.h"
 #include "ui_users_table.h"
+#include "edit_user.h"
 
 users_table::users_table(QWidget *parent) :
-    QWidget(parent),
+    QDialog(parent),
     ui(new Ui::users_table)
 {
     ui->setupUi(this);
+}
+
+void users_table::setUsers(std::vector<user>* m_users_)
+{
+    m_users = m_users_;
+    addColumns();
+    addRows();
 }
 
 users_table::~users_table()
 {
     delete ui;
 }
+
+void users_table::addColumns()
+{
+    tbl = new QStandardItemModel;
+    tbl->setColumnCount(3);
+    tbl->setHorizontalHeaderLabels(QStringList() << "ФИО" << "Номер" << "Стаж");
+    ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableView->setModel(tbl);
+    ui->tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->tableView->setSelectionMode(QAbstractItemView::SingleSelection);
+}
+
+void users_table::addRows()
+{
+    for (size_t i = 0; i < m_users->size(); i++)
+    {
+        if (m_users->at(i).getRole() == 0)
+        {
+            QList<QStandardItem *> standardItemsList;
+            standardItemsList.append(new QStandardItem(m_users->at(i).getName()));
+            standardItemsList.append(new QStandardItem(m_users->at(i).getNumber()));
+            standardItemsList.append(new QStandardItem(m_users->at(i).getDriverExp()));
+            tbl->insertRow(tbl->rowCount(), standardItemsList);
+        }
+    }
+}
+
+
+
+void users_table::on_addButton_clicked()
+{
+
+}
+
+void users_table::on_editButton_clicked()
+{
+    QModelIndexList selection = ui->tableView->selectionModel()->selectedRows();
+    QModelIndex index = selection.at(0);
+    int i = index.row();
+
+    edit_user add_user;
+
+    add_user.setUser(&m_users->at(i));
+    if (add_user.exec() != users_table::Accepted)
+    {
+        return;
+    }
+    setUsers(m_users);
+}
+
+void users_table::on_deleteButton_clicked()
+{
+    QModelIndexList selection = ui->tableView->selectionModel()->selectedRows();
+    QModelIndex index = selection.at(0);
+    size_t ind = index.row();
+    m_users->erase(m_users->begin() + ind);
+    setUsers(m_users);
+}
+
--- lib/users_table.h
+++ lib/users_table.h
@@ -1,22 +1,37 @@
-#ifndef USERS_TABLE_H
-#define USERS_TABLE_H
+#ifndef USERS_TABLE_HPP
+#define USERS_TABLE_HPP
 
-#include <QWidget>
+#include <QDialog>
+#include <QStandardItemModel>
+#include "user.h"
 
 namespace Ui {
 class users_table;
 }
 
-class users_table : public QWidget
+class users_table : public QDialog
 {
     Q_OBJECT
 
 public:
     explicit users_table(QWidget *parent = nullptr);
     ~users_table();
+    void setUsers(std::vector<user>*);
+    void addColumns();
+    void addRows();
+
+private slots:
+
+    void on_addButton_clicked();
+
+    void on_editButton_clicked();
+
+    void on_deleteButton_clicked();
 
 private:
     Ui::users_table *ui;
+    std::vector<user>* m_users;
+    QStandardItemModel *tbl;
 };
 
-#endif // USERS_TABLE_H
+#endif // USERS_TABLE_HPP
